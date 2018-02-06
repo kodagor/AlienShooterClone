b94cedb06c19f2e00d193af9778a71d13439e3f6
Daniel <32864094+kodagor@users.noreply.github.com>
2018-02-06 13:36:55 +0100

    Add files via upload
    Added new levels of speed. After destroy entire fleet, speed level is increasing.

--- settings.py
+++ settings.py
@@ -2,31 +2,47 @@ class Settings():
     """class for game settings"""
 
     def __init__(self):
-        """game setting init"""
+        """static settings"""
         # screen settings
         self.screen_width = 1200
         self.screen_height = 768
         self.bg_color = (0, 0, 0)
 
         # ship settings
-        self.ship_speed_factor = 1.5
         self.ships_limit = 3
 
         # bullet settings
-        self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 230, 230, 230
         self.bullets_allowed = 3
 
         # alien settings
-        self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
-        # fleet_direction = 1 <- right; -1 <- left
-        self.fleet_direction = 1
 
         # stars settings
         # self.star_speed_factor = 1
+
+        # easy change game speed
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """init setting, which are changing during game"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # fleet direction = -1 => left; +1 => right
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """change speed settings"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+
+
 
 
 
